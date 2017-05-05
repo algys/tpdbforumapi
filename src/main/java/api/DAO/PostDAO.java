7279a76a5bca727a17548405daba2c4687dcbafc
Algys Ievlev <ivalgys1997@gmail.com>
2017-05-05 16:54:55 +0300

    Update PostDAO.java

--- src/main/java/api/DAO/PostDAO.java
+++ src/main/java/api/DAO/PostDAO.java
@@ -1,7 +1,7 @@
 package api.DAO;
 
 import api.models.*;
-import javafx.geometry.Pos;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
